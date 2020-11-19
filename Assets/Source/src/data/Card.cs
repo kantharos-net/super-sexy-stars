76d48d86ca501cefd83fa2d536bbe88425e2cbd5
Simone Ferreira <simoneef1986@gmail.com>
2020-11-19 23:53:19 +0000

    added skeleton for folder structure

---
+++ Assets/Source/src/data/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Card
+/// </summary>
+public class Card
+{
+	// TODO: place attributes
+
+	public Card()
+	{
+        // TODO: place constructor with gets/sets
+	}
+}

e47731c8fc816c4759b08d6ad70bfbea961bf425
sdkcouto <matheuscouto.eng@gmail.com>
2020-11-25 18:21:50 -0300

    Tela de inicio done

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene("Start Menu");
+
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadCopyrightScene()
+    {
+        SceneManager.LoadScene("Copyright");
+    }
+    public void LoadRulesScene()
+    {
+        SceneManager.LoadScene("Rules");
+    }
+
+    public void QuitGame()
+    {
+    Application.Quit();
+    }
+}
